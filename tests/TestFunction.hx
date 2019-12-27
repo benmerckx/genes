935a3dae86ce2f057cc74129ed8a8a4482b31bfd
Ben Merckx <ben@codeurs.be>
2019-12-27 14:58:19 +0100

    Emit anonymous function default values

--- tests/TestFunction.hx
+++ tests/TestFunction.hx
@@ -5,36 +5,43 @@ class TestFunction {
   public function new() {}
 
   public function testDefaultValues() {
-    asserts.assert(this.getDefaultInt() == 31);
-    asserts.assert(this.getDefaultInt(32) == 32);
-    asserts.assert(this.getDefaultFloat() == 100.56);
-    asserts.assert(this.getDefaultFloat(101.99) == 101.99);
-    asserts.assert(this.getDefaultBool() == true);
-    asserts.assert(this.getDefaultBool(false) == false);
-    asserts.assert(this.getDefaultString() == ' test ');
-    asserts.assert(this.getDefaultString('testing') == 'testing');
-    asserts.assert(this.getDefaultNull() == null);
-    asserts.assert(this.getDefaultNull(true) == true);
+    asserts.assert(getDefaultInt() == 31);
+    asserts.assert(getDefaultInt(32) == 32);
+    asserts.assert(getDefaultFloat() == 100.56);
+    asserts.assert(getDefaultFloat(101.99) == 101.99);
+    asserts.assert(getDefaultBool() == true);
+    asserts.assert(getDefaultBool(false) == false);
+    asserts.assert(getDefaultString() == ' test ');
+    asserts.assert(getDefaultString('testing') == 'testing');
+    asserts.assert(getDefaultNull() == null);
+    asserts.assert(getDefaultNull(true) == true);
     return asserts.done();
   }
 
-  private function getDefaultInt(a = 31) {
+  public function testAnonymousDefaultValues() {
+    var test = function(a = 1) return a;
+    asserts.assert(test() == 1);
+    asserts.assert(test(2) == 2);
+    return asserts.done();
+  }
+
+  function getDefaultInt(a = 31) {
     return a;
   }
 
-  private function getDefaultFloat(a = 100.56) {
+  function getDefaultFloat(a = 100.56) {
     return a;
   }
 
-  private function getDefaultBool(a = true) {
+  function getDefaultBool(a = true) {
     return a;
   }
 
-  private function getDefaultString(a = ' test ') {
+  function getDefaultString(a = ' test ') {
     return a;
   }
 
-  private function getDefaultNull(a = null) {
+  function getDefaultNull(a = null) {
     return a;
   }
 }
