638a4be112b26f3d0c1cc5e974277c7ad5ad4ee9
Ben Merckx <ben@codeurs.be>
2020-01-24 10:46:10 -0500

    Test native without requires

--- tests/TestReactComponent.hx
+++ tests/TestReactComponent.hx
@@ -13,6 +13,11 @@ private extern class ReactDOMServer {
   public static function renderToString(element: Dynamic): String;
 }
 
+// @:native without a require should work in this case since React was imported
+
+@:native('React.Fragment')
+private extern class Fragment {}
+
 @:jsRequire("react", "Component")
 @:native('React.Component')
 private extern class NativeComponent<State, Props> {
@@ -46,6 +51,7 @@ class TestReactComponent {
   public function testCreateComponent() {
     var vdom = React.createElement(MyComponent);
     asserts.assert(ReactDOMServer.renderToString(vdom) == '2');
+    asserts.assert(Fragment != null);
     return asserts.done();
   }
 }
