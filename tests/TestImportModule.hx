29a0ba345d1298cf3bc778f0f62b4bd55c153ce1
Kevin Leung <kevinresol@gmail.com>
2021-02-11 16:18:42 +0800

    Test name clash

--- tests/TestImportModule.hx
+++ tests/TestImportModule.hx
@@ -3,19 +3,36 @@ package tests;
 import tink.unit.AssertionBuffer;
 import tests.ExternalClass;
 import tests.ExternalClass2;
+import tests.foo.MyClass as FooClass;
+import tests.bar.MyClass as BarClass;
 
+using StringTools;
 using tink.CoreApi;
 
 // TODO: we should probably also make sure the static import statements are not present in the generated js
 @:asserts
 class TestImportModule {
+  var source: String;
+
   public function new() {}
+
+  @:setup
+  public function setup() {
+    final url: String = js.Syntax.code('import.meta.url');
+    return if (url.startsWith('file://')) {
+      source = sys.io.File.getContent(url.substr('file://'.length));
+      Promise.NOISE;
+    } else {
+      Promise.reject(new Error('Unexpected URL: $url'));
+    }
+  }
 
   public function testImportModule(): Promise<AssertionBuffer> {
     return genes.Genes.dynamicImport(ExternalClass -> {
       var a = new ExternalClass();
       asserts.assert(Std.is(a, ExternalClass));
       asserts.assert(ExternalClass.success() == 'success');
+      asserts.assert(!staticallyImported(ExternalClass));
       return asserts.done();
     }).ofJsPromise();
   }
@@ -25,6 +42,7 @@ class TestImportModule {
       var a = new ExternalSubClass();
       asserts.assert(Std.is(a, ExternalSubClass));
       asserts.assert(ExternalSubClass.sub() == 'sub');
+      asserts.assert(!staticallyImported(ExternalSubClass));
       return asserts.done();
     }).ofJsPromise();
   }
@@ -44,8 +62,30 @@ class TestImportModule {
         var a = new ExternalSubClass2();
         asserts.assert(Std.is(a, ExternalSubClass2));
         asserts.assert(ExternalSubClass2.sub() == 'sub2');
+        asserts.assert(!staticallyImported(ExternalClass));
+        asserts.assert(!staticallyImported(ExternalSubClass));
+        asserts.assert(!staticallyImported(ExternalClass2));
+        asserts.assert(!staticallyImported(ExternalSubClass2));
+
         return asserts.done();
       })
       .ofJsPromise();
+  }
+
+  public function testImportAliased(): Promise<AssertionBuffer> {
+    return genes.Genes.dynamicImport((FooClass, BarClass) -> {
+      asserts.assert(new FooClass().toString() == 'foo');
+      asserts.assert(new BarClass().toString() == 'bar');
+      asserts.assert(!staticallyImported(FooClass));
+      asserts.assert(!staticallyImported(BarClass));
+      return asserts.done();
+    }).ofJsPromise();
+  }
+
+  // TODO: this check is quite rough
+  function staticallyImported<T>(cls: Class<T>) {
+    final name = Type.getClassName(cls).split('.').pop();
+    trace('^import {.*$name.*} from ".*"$$');
+    return new EReg('^import {.*$name.*} from ".*"$$', 'm').match(source);
   }
 }
---
+++ tests/foo/MyClass.hx
@@ -0,0 +1,8 @@
+package tests.foo;
+
+class MyClass {
+  public function new() {}
+
+  public function toString()
+    return 'foo';
+}
