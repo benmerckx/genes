d5d79f02d29aefb4dc4ea7c143d972df06b4fd11
Ben Merckx <ben@codeurs.be>
2022-02-15 08:27:15 +0100

    Test interface safe cast

--- tests/TestBoot.hx
+++ tests/TestBoot.hx
@@ -1,12 +1,24 @@
 package tests;
 
+interface MyInterface {}
+
+class MyInstance implements MyInterface {
+  public function new() {}
+}
+
 @:asserts
 class TestBoot {
   public function new() {}
 
-  public function testSafCast() {
+  public function testSafeCast() {
     var a: Dynamic = new TestBoot();
     asserts.assert((cast(a, TestBoot)) == a);
+    return asserts.done();
+  }
+
+  public function testInterfaceCast() {
+    var instance = new MyInstance();
+    asserts.assert((cast(instance, MyInterface)) == instance);
     return asserts.done();
   }
 }
